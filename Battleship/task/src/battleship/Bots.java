3a2c3fd26410c5db41a59328515e67d9e80d3c78
Sergei857 <32968405+Sergei857@users.noreply.github.com>
2022-11-30 23:33:56 +0300

    1.2

--- Battleship/task/src/battleship/Bots.java
+++ Battleship/task/src/battleship/Bots.java
@@ -11,4 +11,44 @@ public class Bots {
                     {"Enter the coordinates of the Destroyer (2 cells):", "2"}
             };
 
+
+    public static int[] CoordinatForInt(String str) throws Exception {
+        try {
+            int[] xy = new int[4];
+            String[] str_xy = str.split(" ");
+
+            switch (str_xy[0].charAt(0)) {
+                case 'A' -> xy[0] = 0;
+                case 'B' -> xy[0] = 1;
+                case 'C' -> xy[0] = 2;
+                case 'D' -> xy[0] = 3;
+                case 'E' -> xy[0] = 4;
+                case 'F' -> xy[0] = 5;
+                case 'G' -> xy[0] = 6;
+                case 'H' -> xy[0] = 7;
+                case 'I' -> xy[0] = 8;
+                case 'J' -> xy[0] = 9;
+            }
+            xy[1] = Integer.parseInt(String.valueOf(new StringBuffer(str_xy[0]).deleteCharAt(0)));
+            switch (str_xy[1].charAt(0)) {
+                case 'A' -> xy[2] = 0;
+                case 'B' -> xy[2] = 1;
+                case 'C' -> xy[2] = 2;
+                case 'D' -> xy[2] = 3;
+                case 'E' -> xy[2] = 4;
+                case 'F' -> xy[2] = 5;
+                case 'G' -> xy[2] = 6;
+                case 'H' -> xy[2] = 7;
+                case 'I' -> xy[2] = 8;
+                case 'J' -> xy[2] = 9;
+            }
+            xy[3] = Integer.parseInt(String.valueOf(new StringBuffer(str_xy[1]).deleteCharAt(0)));
+            return xy;
+        } catch (Exception e) {
+            throw new Exception(e);
+        }
+
+    }
 }
+
+
--- Battleship/task/src/battleship/Main.java
+++ Battleship/task/src/battleship/Main.java
@@ -2,21 +2,31 @@ package battleship;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Pole user_pole = new Pole(Pole.empty_pole);
         PrintPole.print_pole(user_pole); // Создание пустого поля
-        for (String[] bot_for_input : Bots.bots_list) {
-            System.out.print("\n" + bot_for_input[0] + "\n");
+        for (int i = 0; i <= Bots.bots_list.length; i++) {
+            System.out.print("\n" + Bots.bots_list[i][0] + "\n");
+            String str_coorinat = CoordinateInput();
+            //System.out.print(str_coorinat);
+
+            try {
+                System.out.print(Arrays.toString(Bots.CoordinatForInt(str_coorinat)));
+
+            } catch (Exception e) {
+                System.out.print("Error! :\n");
+                i--;
+            }
+
         }
     }
 
-    public static void String() throws Exception {
+    public static String CoordinateInput() throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder stringBuilder = new StringBuilder(reader.readLine());
-        System.out.println(stringBuilder.reverse());
-        reader.close();
+        return reader.readLine();
     }
 }
